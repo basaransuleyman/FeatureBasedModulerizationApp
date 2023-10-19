71975ad59c0a1c932eabcfc698a31c6a26b367f1
Suleyman Basaranoglu <suleyman.basaranoglu@getir.com>
2023-10-19 19:36:56 +0300

    Create Core Module

---
+++ core/build.gradle.kts
@@ -0,0 +1,46 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.library)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.example.core"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.constraint.layout)
+    implementation(libs.hilt.core)
+    implementation(libs.lifecycle.ktx)
+    implementation(libs.glide)
+    kapt(libs.hilt.compiler)
+    implementation(libs.android.core)
+    implementation(libs.appcompat)
+}
