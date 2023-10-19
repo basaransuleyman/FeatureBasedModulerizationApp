054aa7b7b91080fbb9fa4c106702b6f41dc2d092
Suleyman Basaranoglu <suleyman.basaranoglu@getir.com>
2023-10-19 19:40:56 +0300

    Create Data Module

---
+++ data/build.gradle.kts
@@ -0,0 +1,51 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.library)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.example.data"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":domain"))
+
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.hilt.core)
+    kapt(libs.hilt.compiler)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.gson.converter)
+    implementation(libs.rx.java.android)
+    implementation(libs.rx.java.core)
+    implementation(libs.android.core)
+    implementation(libs.appcompat)
+}
